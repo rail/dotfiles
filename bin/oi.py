ed70162c458cd398f7c5aa0532e7006ad2012ceb
Rail Aliiev <rail@mozilla.com>
2016-01-10 21:19:04 -0500

    Reload i3/pystatus configs

--- bin/oi.py
+++ bin/oi.py
@@ -2,11 +2,18 @@
 
 import sh
 import os
+import sys
 
 imap_status = os.path.expanduser("~/bin/of_status")
 os.chmod(imap_status, 0o755)
+print "IMAP filter in progress... ",
+sys.stdout.flush()
 sh.nice("imapfilter", "-c",
         os.path.expanduser("~/.dotfiles/imapfilter.config.lua"),
         _timeout=3 * 60)
+print "Done"
+print "sync in progress... ",
+sys.stdout.flush()
 sh.nice("offlineimap", _timeout=3 * 60)
+print "Done"
 os.chmod(imap_status, 0o644)
